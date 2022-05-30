ef0ac3eeaea518a53bd25d012f6bd85dab320aad
Allaloubouchra <bouchra.allalou10@gmail.com>
2022-05-30 21:31:31 +0200

    center app commit

---
+++ app/src/main/java/com/example/centerapp/AddSurvey.kt
@@ -0,0 +1,11 @@
+package com.example.centerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddSurvey : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_survey)
+    }
+}
---
+++ app/src/main/java/com/example/centerapp/CompleteProfil.kt
@@ -0,0 +1,11 @@
+package com.example.centerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompleteProfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_profil)
+    }
+}
