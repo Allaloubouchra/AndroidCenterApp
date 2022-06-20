6eed09377a582cd514fac1b8b2bf8443137258d9
Simou <wassim.chaguetmi@gmail.com>
2022-06-21 00:27:12 +0100

    implemented validate and cancel on the doctor side

--- app/src/main/java/com/example/centerapp/Survey.kt
+++ app/src/main/java/com/example/centerapp/Survey.kt
@@ -6,24 +6,44 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.centerapp.models.Appointment
-import java.util.*
+import com.example.centerapp.rest.AppointmentService
+import com.example.centerapp.rest.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     lateinit var fulName: EditText
     lateinit var date: EditText
-    lateinit var button_annuler: Button
-    lateinit var RadioGroup1: RadioGroup
-    lateinit var RadioGroup2: RadioGroup
-    lateinit var RadioGroup3: RadioGroup
+    lateinit var buttonAnnuler: Button
+    lateinit var buttonValidate: Button
+    lateinit var buttonCancel: Button
+    lateinit var pathoRadioGroup: RadioGroup
+    lateinit var covidContact: RadioGroup
+    lateinit var covidAffected: RadioGroup
     lateinit var radioButton1: RadioButton
     lateinit var radioButton2: RadioButton
     lateinit var radioButton3: RadioButton
+    lateinit var spinnerTem: Spinner
+    lateinit var spinnerFreqCar: Spinner
+    lateinit var spinnerFreqRes: Spinner
+    lateinit var spinnertens: Spinner
+    lateinit var spinnerSatur: Spinner
+
+
+    var temperature: String? = ""
+    var frequenceCardiaque: String? = ""
+    var frequenceRespi: String? = ""
+    var tension: String? = ""
+    var satur: String? = ""
 
 
     var appointment: Appointment? = null
 
+    val appointmentService: AppointmentService =
+        RetrofitClient.client.create(AppointmentService::class.java)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,36 +52,30 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         supportActionBar?.hide()
 
 
-        button_annuler = findViewById(R.id.annuler)
-        button_annuler.setOnClickListener { startActivity(Intent(this, AddSurvey::class.java)) }
+        buttonAnnuler = findViewById(R.id.annuler)
+        buttonAnnuler.setOnClickListener { finish() }
 
-        RadioGroup1 = findViewById(R.id.radioGroup1)
-        RadioGroup2 = findViewById(R.id.radioGroup2)
-        RadioGroup3 = findViewById(R.id.radioGroup3)
+        buttonValidate = findViewById(R.id.validateButton)
+        buttonValidate.setOnClickListener { validate() }
+
+        buttonCancel = findViewById(R.id.cancelButton)
+        buttonCancel.setOnClickListener { cancel() }
+
+        pathoRadioGroup = findViewById(R.id.pathoRadioGroup)
+        covidContact = findViewById(R.id.covidContact)
+        covidAffected = findViewById(R.id.covidAffected)
+
 
         val selectedId1: Int =
-            RadioGroup1.getCheckedRadioButtonId() // si aucun button radio n'est coché, cette fonction retourne -1
+            pathoRadioGroup.getCheckedRadioButtonId() // si aucun button radio n'est coché, cette fonction retourne -1
         if (selectedId1 != -1) {
             val radioButton1: RadioButton
             radioButton1 = findViewById<View>(selectedId1) as RadioButton
         }
 
-        val selectedId2: Int =
-            RadioGroup2.getCheckedRadioButtonId() // si aucun button radio n'est coché, cette fonction retourne -1
-        if (selectedId2 != -1) {
-            val radioButton1: RadioButton
-            radioButton2 = findViewById<View>(selectedId2) as RadioButton
-        }
 
-        val selectedId3: Int =
-            RadioGroup3.getCheckedRadioButtonId() // si aucun button radio n'est coché, cette fonction retourne -1
-        if (selectedId3 != -1) {
-            val radioButton3: RadioButton
-            radioButton3 = findViewById<View>(selectedId3) as RadioButton
-        }
-
-
-        val spinnerTem: Spinner = findViewById(R.id.spinnerTem)
+        //<editor-fold desc="Description">
+        spinnerTem = findViewById(R.id.spinnerTem)
         ArrayAdapter.createFromResource(
             this,
             R.array.array_temp,
@@ -71,9 +85,20 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerTem.adapter = adapter
         }
+        spinnerTem.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                temperature = if (p2 == 0) "F" else "A"
+            }
 
+            override fun onNothingSelected(p0: AdapterView<*>?) {
 
-        val spinnerFreqCar: Spinner = findViewById(R.id.spinnerFreqCar)
+            }
+
+        }
+        //</editor-fold>
+
+        //<editor-fold desc="freq card">
+        spinnerFreqCar = findViewById(R.id.spinnerFreqCar)
         ArrayAdapter.createFromResource(
             this,
             R.array.array_freq_cardi,
@@ -82,8 +107,24 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerFreqCar.adapter = adapter
         }
+        spinnerFreqCar.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                frequenceCardiaque = when (p2) {
+                    0 -> "T"
+                    1 -> "N"
+                    2 -> "B"
+                    else -> ""
+                }
+            }
 
-        val spinnerFreqRes: Spinner = findViewById(R.id.spinnerFreqRes)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+        //</editor-fold>
+
+        //<editor-fold desc="freq respi">
+        spinnerFreqRes = findViewById(R.id.spinnerFreqRes)
         ArrayAdapter.createFromResource(
             this,
             R.array.array_freq_respi,
@@ -92,8 +133,24 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerFreqRes.adapter = adapter
         }
+        spinnerFreqRes.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                frequenceRespi = when (p2) {
+                    0 -> "E"
+                    1 -> "D"
+                    2 -> "B"
+                    else -> ""
+                }
+            }
 
-        val spinnertens: Spinner = findViewById(R.id.spinnertens)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+        //</editor-fold>
+
+        //<editor-fold desc="tansion">
+        spinnertens = findViewById(R.id.spinnertens)
         ArrayAdapter.createFromResource(
             this,
             R.array.array_tansion,
@@ -102,8 +159,25 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnertens.adapter = adapter
         }
+        spinnertens.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                tension = when (p2) {
+                    0 -> "H"
+                    1 -> "N"
+                    2 -> "O"
+                    else -> ""
+                }
+            }
 
-        val spinnerSatur: Spinner = findViewById(R.id.spinnerSatur)
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+
+        //</editor-fold>
+
+        //<editor-fold desc="satur">
+        spinnerSatur = findViewById(R.id.spinnerSatur)
         ArrayAdapter.createFromResource(
             this,
             R.array.array_satura,
@@ -112,6 +186,16 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinnerSatur.adapter = adapter
         }
+        spinnerSatur.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                satur = if (p2 == 0) "G" else "B"
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+        //</editor-fold>
 
         if (intent.getSerializableExtra("appointment") != null) {
             appointment = intent.getSerializableExtra("appointment") as Appointment
@@ -128,8 +212,8 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     }
 
     override fun onResume() {
-        fulName= findViewById(R.id.fullName)
-        date= findViewById(R.id.dateOfBirth)
+        fulName = findViewById(R.id.fullName)
+        date = findViewById(R.id.dateOfBirth)
         fulName.setText(appointment?.patient?.fullName.toString())
         date.setText(appointment?.appointmentDate.toString())
 
@@ -142,5 +226,81 @@ open class Survey : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         TODO("Not yet implemented")
+    }
+
+    private fun validateData(): Map<String, Any?> {
+        val data = mutableMapOf<String, Any?>()
+        data["positive_covid"] =
+            covidAffected.checkedRadioButtonId == R.id.covidAffectedYes
+        data["contamination"] = covidContact.checkedRadioButtonId == R.id.covidContactYes
+        // todo handle the disease cases
+        data["temperature"] = temperature
+        data["heart_rate"] = frequenceCardiaque
+        data["respiratory_rate"] = frequenceRespi
+        data["blood_pressure"] = tension
+        data["oximetry"] = satur
+        data["vaccination_appointment"] = appointment?.id
+
+        return data
+    }
+
+    private fun validate() {
+        val data = validateData()
+        appointmentService.validateAppointment(appointment!!.id, data)
+            .enqueue(object : Callback<Void> {
+                override fun onResponse(
+                    call: Call<Void>,
+                    response: Response<Void>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Vaccination validé avec succés",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    t.printStackTrace()
+                    Toast.makeText(
+                        applicationContext,
+                        "Network error",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            })
+    }
+
+    private fun cancel() {
+        val data = validateData()
+        appointmentService.cancelAppointment(appointment!!.id, data)
+            .enqueue(object : Callback<Void> {
+                override fun onResponse(
+                    call: Call<Void>,
+                    response: Response<Void>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Vaccination annulé avec succés",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                    t.printStackTrace()
+                    Toast.makeText(
+                        applicationContext,
+                        "Network error",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+            })
     }
 }
--- app/src/main/java/com/example/centerapp/rest/AppointmentService.kt
+++ app/src/main/java/com/example/centerapp/rest/AppointmentService.kt
@@ -2,11 +2,29 @@ package com.example.centerapp.rest
 
 import com.example.centerapp.models.Appointment
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.Response
+import retrofit2.http.*
 
 interface AppointmentService {
     @GET("vaccination-appointment/{id}/")
     fun getAppointmentById(@Path("id") id: Long): Call<Appointment>
+
+    @PATCH("vaccination-appointment/{id}/")
+    @JvmSuppressWildcards
+    fun updateAppointment(@Path("id") id: Long, @Body() data: Map<String, Any>): Call<Appointment>
+
+    @POST("vaccination-appointment/{id}/validate/")
+    @JvmSuppressWildcards
+    fun validateAppointment(
+        @Path("id") id: Long,
+        @Body() data: Map<String, Any?>
+    ): Call<Void>
+
+    @POST("vaccination-appointment/{id}/cancel/")
+    @JvmSuppressWildcards
+    fun cancelAppointment(
+        @Path("id") id: Long,
+        @Body() data: Map<String, Any?>
+    ): Call<Void>
 
 }
